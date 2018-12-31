e039864d4760a5f0c03dae67033b379774adbb2c
Daniel Corrêa <danccorrrea@gmail.com>
2018-12-31 02:17:33 -0200

    Finalizado

--- app/src/main/java/com/dscorrea/conversormoedas/MainActivity.kt
+++ app/src/main/java/com/dscorrea/conversormoedas/MainActivity.kt
@@ -2,31 +2,49 @@ package com.dscorrea.conversormoedas
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val spinner = findViewById(R.id.spnMoeda) as Spinner
+        val entradaValor = findViewById<EditText>(R.id.entrada_valor)
+        val spinnerMoeda = findViewById<Spinner>(R.id.spinner_moeda)
+        val textoResultado = findViewById<TextView>(R.id.texto_resultado)
+        val botaoConverter = findViewById<Button>(R.id.botao_converter)
+
         val adapter = ArrayAdapter.createFromResource(this, R.array.array_moedas, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.setAdapter(adapter)
 
-        btnConverter.setOnClickListener {
+        spinnerMoeda.adapter = adapter
 
-//            val valorConvertido = when (spinner.selectedItem) {
-//                "Dolar" -> 4.00
-//                "Euro" -> 4.50
-//                "Peso" ->  0.10
-//            }
+        botaoConverter.setOnClickListener {
 
-            val valorConvertido = lblValor.text.toString().toDouble() / 3.88
-            lblResultado.setText("%.2f".format(valorConvertido))
+            var valorDigitado = entradaValor.text.toString().toDouble()
+            var moedaEscolhida = spinnerMoeda.selectedItemPosition
+            var valorConvertido = 0.00
+
+
+            when (moedaEscolhida) {
+                0 -> {
+                    valorConvertido = valorDigitado / 3.88
+                    textoResultado.text = "R$" + valorDigitado + " = US$" + "%.2f".format(valorConvertido)
+                }
+                1 -> {
+                    valorConvertido = valorDigitado / 4.44
+                    textoResultado.text = "R$" + valorDigitado + " = €" + "%.2f".format(valorConvertido)
+                }
+                2 -> {
+                    valorConvertido = valorDigitado * 9.71
+                    textoResultado.text = "R$" + valorDigitado + " = $" + "%.2f".format(valorConvertido)
+                }
+            }
+
         }
+
+
     }
 }
